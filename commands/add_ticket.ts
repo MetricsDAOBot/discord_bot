39c26e53f1805e88c56914804610703c21696a03
kida <leslug.away@gmail.com>
2023-02-13 21:33:57 +0800

    Added manual ticket addition by admin

---
+++ commands/add_ticket.ts
@@ -0,0 +1,43 @@
+import { CacheType, ChatInputCommandInteraction } from "discord.js";
+import { SlashCommandBuilder } from'discord.js';
+import axios from '../services/axios';
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('add_ticket')
+		.setDescription('Adds a Golden Ticket to a user!')
+		.addUserOption(option =>
+			option.setName('user')
+				.setDescription('Discord User')
+                .setRequired(true))
+		.addStringOption(option =>
+			option.setName('remark')
+				.setDescription('Add a remark'))
+		.addNumberOption(option =>
+			option.setName('number_of_tickets')
+				.setDescription('How many tickets to give this user?')),
+	async execute(interaction: ChatInputCommandInteraction<CacheType>) {
+        try {
+            let user = interaction.options.getUser('user');
+            let remark = interaction.options.getString('remark');
+            let number_of_tickets = interaction.options.getNumber('number_of_tickets');
+            let res = await axios.post(
+                '/add_ticket', 
+                { 
+                    discord_id: user?.id, 
+                    discord_name: `${user?.username}#${user?.discriminator}`, 
+                    created_by: `${interaction.user.username}#${interaction.user.discriminator}`, 
+                    created_by_id: interaction.user.id,
+                    remark,
+                    number_of_tickets
+                });
+
+            await interaction.reply({ content: res.data, ephemeral: true });
+        }
+
+        catch (e){
+            //console.log(e);
+            await interaction.reply({ content: "Error adding tickets!", ephemeral: true });
+        }
+	},
+};
