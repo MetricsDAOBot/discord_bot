1083987ce6e6f430b1dce3fa36e6f6f83d165cf6
kida <leslug.away@gmail.com>
2023-04-10 22:20:25 +0800

    Now escapes quotes and new lines

--- commands/all_requests.ts
+++ commands/all_requests.ts
@@ -28,7 +28,14 @@ module.exports = {
             }
 
             res.data.forEach(request => {
-                const {discord_name, created_at, updated_at, regraded_at, approved_at, uuid, is_regrading, submission, grader_feedback, current_score, expected_score, reason, regraded_score, regraded_reason, regraded_by} = request;
+                let {discord_name, created_at, updated_at, regraded_at, approved_at, uuid, is_regrading, submission, grader_feedback, current_score, expected_score, reason, regraded_score, regraded_reason, regraded_by} = request;
+                
+                // escape everything
+                submission = submission? submission.replace('"', '""').replace('\n', ' ').replace("'", "''") : "";
+                grader_feedback = grader_feedback? grader_feedback.replace('"', '""').replace('\n', ' ').replace("'", "''") : "";
+                reason = reason? reason.replace('"', '""').replace('\n', ' ').replace("'", "''") : "";
+                regraded_reason = regraded_reason? regraded_reason.replace('"', '""').replace('\n', ' ').replace("'", "''") : "";
+                
                 ret += `\n"${discord_name}","${created_at}","${updated_at}","${regraded_at ?? "Not Regraded"}","${approved_at ?? "Not Approved"}","${uuid}","${is_regrading? "Yes" : "No"}","${submission ?? "-"}","${grader_feedback ?? "-"}","${current_score ?? "-"}","${expected_score ?? "-"}","${reason ?? "-"}","${regraded_score ?? "-"}","${regraded_reason ?? "-"}"`;
                 if(shouldIncludeRegrader) {
                     ret += `,"${regraded_by ?? 'Not Regraded'}"`;
